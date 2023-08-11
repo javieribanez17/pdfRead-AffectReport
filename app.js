67412c16f1b876dfc2552654a27d1ead8a83d427
JavierIbanezB <Javier.Ibanez@ey.co.com>
2023-08-11 11:32:08 -0500

    Extracción de datos de pdf estrcuturados en tabla

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const bodyParser = require("body-parser");
 const multer = require("multer");
 const fs = require("fs");
 const pdf = require("pdf-parse");
-//Paquetes de Lanchain
+//Paquetes de Lanchain para leer pdf
 const { OpenAI } = require("langchain/llms/openai");
 const { RetrievalQAChain } = require("langchain/chains");
 const { HNSWLib } = require("langchain/vectorstores/hnswlib");
 const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
 const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
+//Paquetes de LangChain adicionales para extraer datos de Affect Report
+const { PromptTemplate } = require("langchain/prompts");
+const { LLMChain } = require("langchain/chains");
+const { StructuredOutputParser } = require("langchain/output_parsers");
+const { z } = require("zod");
+//Declaraciones necesarias
+require("dotenv").config();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
 //Archivos
 /*const upload = multer({
     dest: 'uploads'
@@ -25,17 +36,78 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
-//Declaraciones necesarias
-require("dotenv").config();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 3000;
 //Variables globales
 let respondModel = "";
+let respondModelA = [];
 let consultPrev = "";
-let chain = "";
 let pdfTitle = "";
+let qModel = "";
+let jsonArray = [];
+//Extraccion de datos AR
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    paciente: z.object({
+      triage: z
+        .string()
+        .describe(
+          "Si el paciente esta en peligro de muerte:{Critico}\nSi requiere hospitalización:{Urgente}\nSi no es ni Critico ni urgente:{General}"
+        ),
+      nombre: z.string().describe("Nombre del paciente"),
+      genero: z.string().describe("¿El paciente es hombre o mujer?"),
+      edad: z.string().describe("Edad del paciente al presentar la queja"),
+      nacimiento: z.string().describe("Fecha de nacimiento"),
+      altura: z.string().describe("Estatura"),
+      peso: z.string().describe("Peso"),
+    }),
+    descripcion: z.object({
+      indicacion: z.string().describe("Indicación médica del paciente"),
+      medicacion: z
+        .array(z.string())
+        .describe("Medicación previa del paciente"),
+      id: z.string().describe("¿Cuál es el Patient ID?"),
+      medicamentos: z
+        .array(z.string())
+        .describe("Medicamentos consumidos por el paciente"),
+      via: z.string().describe("Vía de administración del medicamento"),
+      dosis: z.string().describe("Dosis del medicamento consumida"),
+      sintomas: z.array(z.string()).describe("Sintomas del paciente"),
+    }),
+    producto: z.object({
+      nombre: z.string().describe("Nombre del producto sospechoso"),
+      lugar: z.string().describe("Dónde fue comprado el prodcuto"),
+    }),
+    informante: z.object({
+      tipo: z
+        .string()
+        .describe("Relación de quien hace el reporte con el paciente"),
+      nombre: z.string().describe("Nombre del informante"),
+      pais: z.string().describe("País desde donde reporta"),
+    }),
+    fechas: z.object({
+      notificacion: z.string().describe("¿Cuando realizó el primer reporte?"),
+      actual: z.string().describe("Fecha del reporte actual"),
+      uso: z
+        .string()
+        .describe("¿Cuando empezó el paciente a consumir el medicamento?"),
+    }),
+  })
+);
+const formatInstructions = parser.getFormatInstructions();
+const prompt = new PromptTemplate({
+  template:
+    "Extrae y clasifica la siguiente información:\n{format_instructions}\nA partir del texto:\n{text}\nSi no encuentras algunos datos dentro del texto deja su valor vacío excepto en 'triage'",
+  inputVariables: ["text"],
+  partialVariables: { format_instructions: formatInstructions },
+});
+const model = new OpenAI({
+  modelName: "gpt-3.5-turbo",
+  openAIApiKey: process.env.OPEN_AI_KEY,
+  temperature: 0,
+});
+const chain = new LLMChain({
+  llm: model,
+  prompt: prompt,
+});
 //Certificado deshabilitado
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 //--------------------------------Servidor------------------------------------------
@@ -60,69 +132,44 @@ app.post("/load", upload.single("loadFile"), async (req, res) => {
     contentPdf = data.text;
     //res.send(contentPdf);
   });
-  //Modelo al cual se hace la pregunta del usuario
-  const model = new OpenAI({
-    openAIApiKey: process.env.OPEN_AI_KEY,
-  });
-  //Configuración del metodo para dividir textos
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    separator: "\n",
-    chunkSize: 1000,
-    chunkOverlap: 200,
-    lengthFunction: (contentPdf) => contentPdf.length,
-  });
-  //División del texto del PDF en pedazos
-  const chunks = await textSplitter.splitText(contentPdf);
-  const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.OPEN_AI_KEY,
-  });
-  //Creación de la tienda de vectores o index
-  vectorStore = await HNSWLib.fromTexts(chunks, [], embeddings);
-  //Inicializa un retriever alrededor de la tienda de vectores
-  const vectorStoreRetriever = await vectorStore.asRetriever();
-  //Creación del chain que usa el modelo LLM y el retriver de la tienda de vectores
-  chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
-  /*Busqueda en la base de conocimiento de la pregunta del usuario
-    const docs = await vectorStore.similaritySearch(req.body.questionModel);*/
+  const reports = contentPdf.split(/\n\s*\n/);
+
+  for (const report of reports) {
+    if (report != "") {
+      try {
+        qModel = await prompt.format({
+          text: report,
+        });
+        respondModel = await chain.call({ text: qModel });
+        console.log("llegue acá");
+        jsonOutputM = await JSON.parse(respondModel.text);
+        respondModelA.push(jsonOutputM);
+      } catch (err) {
+        console.log(
+          "Hubo un error al comunicarse con el modelo de OpenAI: " + err
+        );
+      }
+    }
+  }
+
   res.render("home", {
     pdfTitle: pdfTitle,
-    respondModel: respondModel,
+    paciente: respondModelA,
     consultPrev: consultPrev,
   });
 });
 //GPT request
 app.post("/gpt", async (req, res) => {
-  //Llama la cadena dando en el prompt el contexto y la pregunta
   const response = await chain.call({
     query: req.body.questionModel,
   });
   res.render("home", {
     pdfTitle: pdfTitle,
-    respondModel: response.text,
+    paciente: respondModelA,
     consultPrev: req.body.questionModel,
   });
 });
-//Test
-/*app.post("/test", upload.single("loadFile"), async (req, res) => {
-    //Variables locales
-    let contentPdf = "";
-    let pdfTitle = req.file.originalname;
-    let pdfPath = req.file.path;
-    //Lectura del PDF
-    const databuffer = fs.readFileSync(pdfPath);
-    await pdf(databuffer).then(function (data) {
-        contentPdf = data.text;
-        //res.send(contentPdf);
-    });
-    res.render("home", {
-        pdfTitle: pdfTitle,
-        respondModel: respondModel,
-        consultPrev: consultPrev
-    });
-});*/
-app.get("/prueba", (req, res) => {
-  res.render("branch");
-});
+
 app.listen(PORT, function () {
   console.log("Servidor corriendo en el puerto 3000");
 });
